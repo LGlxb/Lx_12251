3e3542cdc8fdc353134f2da659127c6bfdc3fdb3
LGlxb <44798106+LGlxb@users.noreply.github.com>
2018-12-25 19:04:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lg/lx_1225/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lg.lx_1225;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
